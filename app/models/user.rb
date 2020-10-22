1f89569f2d74d6f2883b4c511438db7778372d39
marcusLau <marcus.lau76@gmail.com>
2020-10-21 17:49:41 -0700

    Removed class from User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   has_many :comments 
-  has_many :commented_gears, through: :comments, foreign_key: "user_id", class_name: "Comment" 
-  has_many :owned_gears, foreign_key: "user_id", class_name: "Gear" 
+  has_many :commented_gears, through: :comments, foreign_key: "user_id"
+  has_many :owned_gears, foreign_key: "user_id"
   has_secure_password
 
   def self.find_or_create_by_facebook(auth)
